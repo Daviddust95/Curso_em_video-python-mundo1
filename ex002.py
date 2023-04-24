08df300500fe4049fece946635177bffdfd0623a
Alisson David de Melo <124353154+Daviddust95@users.noreply.github.com>
2023-04-24 17:04:10 -0300

    Add files via upload
    Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.

---
+++ ex002.py
@@ -0,0 +1,7 @@
+'''Exercício Python 002: Faça um programa que
+leia o nome de uma pessoa e mostre uma mensagem
+de boas-vindas.
+ '''
+
+nome = input (" Qual é o seu nome ? ")
+print (f"Seja bem vindo (a)  , {nome}")
