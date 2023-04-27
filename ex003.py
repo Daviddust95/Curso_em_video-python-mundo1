7118a9666a279310da04e82bda96ca08901605df
Alisson David de Melo <124353154+Daviddust95@users.noreply.github.com>
2023-04-27 10:11:40 -0300

    ex003.py

--- ex003.py
+++ ex003.py
@@ -1,7 +1,6 @@
 ''' Desafio 003: Crie um programa que leia dois
  números e mostre a soma entre eles.'''
 
-
 num1 = int(input("Informe um número: "))
 num2 = int(input("Digite outro número: "))
 soma = num1 + num2
